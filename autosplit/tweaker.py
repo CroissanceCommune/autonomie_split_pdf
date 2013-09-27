edaeb5d2b631a610948e6217e07ff5b03fb567a3
Feth AREZKI <feth@tuttu.info>
2013-09-27 09:31:42 +0200

    complete rework of parsing. not done yet
    only handles tresorerie, and only first page

--- autosplit/tweaker.py
+++ autosplit/tweaker.py
@@ -4,11 +4,15 @@ import os.path
 import re
 import time
 import unicodedata
+from collections import Iterable, defaultdict
 
+from pdfminer.pdftypes import resolve1
+from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, process_pdf
 from pdfminer.converter import XMLConverter
 from pdfminer.layout import LAParams
-from pyPdf import PdfFileWriter, PdfFileReader
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from PyPDF2.pdf import Destination
 
 from .file_operations import mkdir_p
 from .log_config import mk_logger
@@ -34,6 +38,7 @@ class PdfTweaker(object):
         self.laparams = LAParams()
         self.year = year
         self.month = month
+        self.last_print_page = 0
         self.output_dir = os.path.join(self._DOCTYPE, self.year, self.month)
         self.pages_to_process = self.config.getvalue('restrict')
         for index, (name, expr) in enumerate(self._XPATH_EXPR.iteritems()):
@@ -84,6 +89,128 @@ class PdfTweaker(object):
         self.logger.info("Wrote %s", output_file)
 
     def tweak(self, pdfstream):
+        mkdir_p(self.output_dir, self.logger)
+        with open(pdfstream.name, 'r') as duplicate_pdfstream:
+            inputpdf = PdfFileReader(duplicate_pdfstream)
+
+            pages_nb = inputpdf.getNumPages()
+            if not self.pages_to_process:
+                # 0 means no restriction
+                self.pages_to_process = pages_nb
+
+            self.logger.info("%s has %d pages", pdfstream.name, pages_nb)
+            self.logger.info("Estimated time for completion of %d pages on "
+            "an average computer: %.f seconds. Please stand while the parsing"
+            " takes place.", self.pages_to_process, self._UNITARY_TIME*self.pages_to_process)
+
+            start = time.clock()
+
+            with open(pdfstream.name, 'rb') as duplicate_pdfstream:
+                reader = PdfFileReader(duplicate_pdfstream)
+                outlines = reader.getOutlines()
+                self.allpages = []
+                for index in xrange(pages_nb):
+                    current_page = reader.getPage(index)
+                    self.allpages.append(current_page)
+                self.alldata = []
+                for entrepreneur, ancode in self.browse(outlines):
+                    self.alldata.append((entrepreneur, ancode))
+
+                cur_entr, cur_ancode = self.alldata[0]
+                next_entr, next_ancode = self.alldata[1]
+                outfname = '%s_%s.pdf' % (cur_entr, cur_ancode)
+                outfname = "%s/%s" % (self.output_dir, outfname.replace('/', '_'))
+
+                next_startpage = self.findpage(next_ancode)
+                if next_startpage is None:
+                    # let's print all remaining pages together and stop the
+                    # program.
+                    pass
+                else:
+                    output = PdfFileWriter()
+                    if self.last_print_page == next_startpage:
+                        self.logger.warning("2 analytic codes on page %d",
+                            self.last_print_page)
+                        output.addPage(self.allpages[self.last_print_page])
+                    else:
+                        for page in self.allpages[self.last_print_page:next_startpage]:
+                            output.addPage(page)
+                            self.last_print_page += 1
+                    with open(outfname, 'w') as wfd:
+                        self.logger.info("|| |-> %s", outfname)
+                        output.write(wfd)
+
+
+
+
+
+
+                if False:
+                    output = PdfFileWriter()
+                    target_page = destination.page.getObject()
+                    output.addPage(target_page)
+                    startoffset = self.findpage(ancode)
+                    print "startoffset", startoffset
+                    outfname = '%s_%s.pdf' % (entrepreneur, ancode)
+                    outfname = "playground/%s" % outfname.replace('/', '_')
+                    with open(outfname, 'w') as wfd:
+                        self.logger.info("|| |-> %s", outfname)
+                        output.write(wfd)
+    #                    destination.page.getObject().writeToStream(wfd, '')
+
+
+            duration = time.clock() - start
+            self.logger.info("Total duration: %s seconds, thank you for your patience",
+                duration)
+
+    def printpage(self):
+        pass
+        self.last_print_page += 1
+
+    def findpage(self, ancode):
+        for index, page in enumerate(self.allpages[self.last_print_page:]):
+            text = page.extractText()
+            if ancode in text:
+                return self.allpages.index(page)
+            if index > 10:
+                self.logger.info("Browsed 10 pages without finding code %s,"
+                "search aborted", ancode)
+                return None
+        return None
+
+
+    def browse(self, outline,
+        level=0,
+        maintitle='',
+        entrepreneur=''):
+        """
+        Recurses through outlines, yielding only when we have anaytic codes
+
+        Yields tuple: entrepreneur, analytic code
+        """
+        for destination in outline:
+            if isinstance(destination, Destination):
+                title = destination.title
+                if level == 2:
+                    yield entrepreneur, title
+                    continue
+                if level == 0:
+                    maintitle = title
+                elif level == 1:
+                    entrepreneur = title
+                self.logger.info("||%s- %s", ' '*level, title)
+            else:
+                if isinstance(destination, Iterable):
+                    for item in self.browse(destination,
+                        level + 1,
+                        maintitle,
+                        entrepreneur):
+                        yield item
+                else:
+                    self.logger.warning("Skipping entry of type %s" %
+                        type(destination))
+
+    def _tweak(self, pdfstream):
         # XXX optim: parse in separate processes/threads
 
         mkdir_p(self.output_dir, self.logger)
