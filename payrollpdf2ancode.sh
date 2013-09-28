ae86fadf528b375abe9c14e46c51f240cbe98889
Feth AREZKI <feth@tuttu.info>
2013-09-28 11:23:04 +0200

    payroll: try hard recovering person name
    still not rock solid.
    If not fetched on line 12, try line 14...

--- payrollpdf2ancode.sh
+++ payrollpdf2ancode.sh
@@ -7,9 +7,12 @@ FILE=$1
 PAGE=$2
 outfile=`tempfile`
 pdftotext -layout -f $PAGE -l $PAGE "${FILE}" $outfile 2>&1 > /dev/null
-echo -n "ANCODE "
-awk 'NR==12 {print;}' $outfile | cut -c 71- | tr -d ' '
-echo -n "NAME "
-awk 'NR==15 {print;}' $outfile | cut -c 125- |sed 's/ *$//'
 
+ANCODE=`awk 'NR==12 {print;}' $outfile | cut -c 71- | tr -d ' '`
+echo "ANCODE $ANCODE"
+NAME=`awk 'NR==15 {print;}' $outfile | cut -c 125- |sed 's/ *$//'`
+if [ x$NAME = x ] ; then # try next line
+    NAME=`awk 'NR==14 {print;}' $outfile | cut -c 125- |sed 's/ *$//'`
+fi
+echo "NAME $NAME"
 rm $outfile
