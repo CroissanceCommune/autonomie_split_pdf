22f40950d68ac7a8c1026263960a224f0f32478f
Feth AREZKI <feth@tuttu.info>
2013-10-23 22:55:15 +0200

    close refactoring by removing killed code

--- autosplit/tweaker_base.py
+++ autosplit/tweaker_base.py
@@ -170,24 +170,6 @@ class PdfTweaker(object):
         default implementation returns empty tuple."""
         return ()
 
-    def old_split_stream(self, pages_nb):
-        cur_index = 0
-        next_index = 1
-        # last_print_page is updated by addpages()
-        outputs_nb = len(self.alldata)
-        for iteration in xrange(outputs_nb + 1):
-            printdata = self.getprintdata(next_index)
-            yield (cur_index,) + printdata
-            cur_index = next_index
-            next_index += 1
-            if self.restrict and self.last_print_page >= self.restrict:
-                self.logger.info(
-                    "Stopping the parsing as requested by limit of %d pages"
-                    " or next section beginning",
-                    self.restrict
-                    )
-                return
-
     def split_stream(self, pages_nb):
         return iter(self.outlinedata)
 
@@ -222,7 +204,7 @@ class OutlineTweaker(PdfTweaker):
         outlines = reader.getOutlines()
 
         self.logger.info("Parsing outlines. Output below")
-        recursive_outlines = self.better_browse(outlines, make_offset=True)
+        recursive_outlines = self.browse(outlines, make_offset=True)
         for first_level_section in recursive_outlines:
             if not first_level_section.subsections:
                 continue
@@ -236,36 +218,6 @@ class OutlineTweaker(PdfTweaker):
             entre_nb + 1, len(self.alldata))
         self.logger.info("ETA: %s s", len(self.alldata) * 0.4)
         return True
-        for index, (level, data) in enumerate(self.browse(outlines)):
-            if level == 0:
-                destination = data
-                section_pages.append(destination.page.idnum)
-                continue
-            elif level != 2:
-                continue
-            entrepreneur, ancode, page = data
-            if self.offset == -1:
-                # assumes there is no header
-                self.offset = page.idnum
-                self.logger.info("Page offset seems to be %i", self.offset)
-            self.alldata.append((entrepreneur, ancode))
-
-        if self.offset == -1:
-            offset = 0
-        else:
-            offset = self.offset
-
-        # beware, we speak of 0 indexed pages
-        self.section_pages = [value - offset for value in section_pages]
-
-        # perhaps this is buggy but should always work with sage
-        self.logger.info("Sections pages seem to be: %s", self.section_pages)
-
-        if self.alldata:
-            return True
-
-        self.logger.critical("could not parse outlines?!")
-        return False
 
     def get_section_boundaries(self):
         assert self.section_pages
@@ -331,40 +283,6 @@ class OutlineTweaker(PdfTweaker):
                 return None
         return None
 
-    def old_addpages(self, output, current_page, print_all_remaining, next_startpage):
-        """
-        :arg bool print_all_remaining: should we print all the remainder (stop
-        splitting)
-        :arg int next_startpage: page until which we print
-
-        I think there is a bug in the content of alldata, but it seems to work
-        """
-
-        print "addpages:", output, current_page, print_all_remaining, next_startpage
-        if print_all_remaining:
-            index = 0 # may be undefined this is a fallback value- fixme
-            for index, page in enumerate(
-                self.allpages[self.last_print_page:self.restrict]):
-                output.addPage(page)
-                self.last_print_page += 1
-            return index + 1
-
-        if self.last_print_page == next_startpage:
-            self.logger.warning(
-                "2 analytic codes on page %d (%s and %s)",
-                self.last_print_page, *[
-                    self.alldata[pnum][1] for pnum in (self.last_print_page,
-                    self.last_print_page + 1)
-                    ]
-                )
-            output.addPage(self.allpages[self.last_print_page])
-            return 1
-
-        for page in self.allpages[self.last_print_page:next_startpage]:
-            output.addPage(page)
-            self.last_print_page += 1
-        return next_startpage - self.last_print_page + 1
-
     def addpages(self, output, startpage, pages_nb, ancode, entrepreneur, reader):
         for pageno in xrange(pages_nb):
             self.last_print_page = startpage + pageno
@@ -380,7 +298,7 @@ class OutlineTweaker(PdfTweaker):
         """
         pass
 
-    def better_browse(self, outline, level=0, make_offset=False):
+    def browse(self, outline, level=0, make_offset=False):
         """
         Offset will be calculated once, on the first outline
 
@@ -406,7 +324,7 @@ class OutlineTweaker(PdfTweaker):
                     previous_section.compute_pagenb(pageno)
                 previous_section = section
             elif isinstance(destination, Iterable):
-                lower_level_sections = self.better_browse(destination, level + 1)
+                lower_level_sections = self.browse(destination, level + 1)
                 previous_section.add_subsections(lower_level_sections)
             else:
                 self.logger.critical(
@@ -415,57 +333,4 @@ class OutlineTweaker(PdfTweaker):
                     )
         return start_ends
 
-
-    def browse(
-            self,
-            outline,
-            level=0,
-            maintitle='',
-            entrepreneur=''
-            ):
-        """
-        Recurses through outlines, yielding only when we have anaytic codes
-
-        Yields tuple: entrepreneur, analytic code
-        """
-        section = None
-        last_seen_section = None
-        for destination in outline:
-            if section is not None:
-                last_seen_section = section
-            if isinstance(destination, Destination):
-                page = destination.page
-                title = destination.title
-                if level == 2:
-                    section = Section(level, page.idnum, entrepreneur)
-                    last_seen_section.set_endpage(section.startpage)
-                    self.section_starts.append(section)
-                    continue
-                if level == 0:
-                    section = Section(level, page.idnum)
-                    self.section_starts.append(section)
-                    print 'appended level 0'
-                    maintitle = title
- #                   yield level, destination
-                elif level == 1:
-                    self.section_starts.append(Section(level, page.idnum,
-                                entrepreneur))
-                    entrepreneur = title
- #                   yield level, destination
-                self.logger.info("%s- %s", '|'*(level + 1), title)
-                return section
-            else:
-                if isinstance(destination, Iterable):
-                    self.browse(
-                            destination,
-                            level + 1,
-                            maintitle,
-                            entrepreneur
-                            )
-  #                      yield item
-                else:
-                    self.logger.warning(
-                        "Skipping entry of type %s" %
-                        type(destination)
-                        )
 
