c114b05b1d1ccc68ee50c8307bc52a7db5ee441e
Feth AREZKI <feth@tuttu.info>
2013-11-18 17:24:28 +0100

    tolerate alternate line for the name in a payroll.

--- payrollpdf2ancode.sh
+++ payrollpdf2ancode.sh
@@ -12,6 +12,8 @@ ANCODE_LINE=11
 ANCODE_COL=55
 NAME_COL=90
 NAME_LINE=9
+# the same as above
+ALTERNATE_NAME_LINE=9
 
 SPECIFIC_CONFIG="$HOME/payroll_rc"
 
@@ -25,7 +27,17 @@ pdftotext -layout -f $PAGE -l $PAGE "${FILE}" $outfile 2>&1 > /dev/null
 ANCODE=`awk "NR==${ANCODE_LINE} {print;}" $outfile |cut -c ${ANCODE_COL}- |sed -e 's/^ \+//' -e 's/ .*//'`
 echo "ANCODE $ANCODE"
 
-NAME=`awk "NR==${NAME_LINE} {print;}" $outfile | cut -c ${NAME_COL}- |sed 's/ *$//'`
-NAME=`echo ${NAME}|sed -e 's/^ \+//' -e 's/^Mme \+//' -e 's/^Mlle \+//' -e 's/^M \+//'`
+function getname() {
+    _NAME_LINE=${1}
+    NAME=`awk "NR==${_NAME_LINE} {print;}" $outfile | cut -c ${NAME_COL}- |sed 's/ *$//'`
+    NAME=`echo ${NAME}|sed -e 's/^ \+//' -e 's/^Mme \+//' -e 's/^Mlle \+//' -e 's/^M \+//'`
+}
+
+getname ${NAME_LINE}
+if [ "x${NAME}" == "x" ]
+    then
+    getname ${ALTERNATE_NAME_LINE}
+fi
+
 echo "NAME $NAME"
-rm $outfile
+rm ${outfile}
