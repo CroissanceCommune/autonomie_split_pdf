a816edd7abdb6c9e498c40609798cd2011d353d3
Feth AREZKI <feth@tuttu.info>
2015-01-05 19:01:52 +0100

    Support alternate ancode lines in payrollpdf2ancode.sh

--- payrollpdf2ancode.sh
+++ payrollpdf2ancode.sh
@@ -9,7 +9,9 @@ outfile=`tempfile`
 
 # Overwritten in specific config
 ANCODE_LINE=11
-ANCODE_COL=55
+# the same as above
+ALTERNATE_ANCODE_LINE=11
+ANCODE_COL=45
 NAME_COL=90
 NAME_LINE=9
 # the same as above
@@ -24,14 +26,26 @@ fi
 
 pdftotext -q -layout -f $PAGE -l $PAGE "${FILE}" $outfile
 
-ANCODE=`awk "NR==${ANCODE_LINE} {print;}" $outfile |cut -c ${ANCODE_COL}- |sed -e 's/^ \+//' -e 's/ .*//'`
-echo "ANCODE $ANCODE"
+function getancode() {
+    _ANCODE_LINE=${1}
+    ANCODE=`awk "NR==${_ANCODE_LINE} {print;}" $outfile |cut -c ${ANCODE_COL}- |sed -e 's/^ \+//' -e 's/ .*//'`
+    ANCODE=`echo ${ANCODE}|sed -e 's/^ \+//'`
+}
 
 function getname() {
     _NAME_LINE=${1}
     NAME=`awk "NR==${_NAME_LINE} {print;}" $outfile | cut -c ${NAME_COL}- |sed 's/ *$//'`
     NAME=`echo ${NAME}|sed -e 's/^ \+//' -e 's/^Mme \+//' -e 's/^Mlle \+//' -e 's/^M \+//'`
 }
+
+getancode ${ANCODE_LINE}
+
+if [ "x${ANCODE}" == "x" ]
+    then
+    getancode ${ALTERNATE_ANCODE_LINE}
+fi
+
+echo "ANCODE $ANCODE"
 
 getname ${NAME_LINE}
 if [ "x${NAME}" == "x" ]
